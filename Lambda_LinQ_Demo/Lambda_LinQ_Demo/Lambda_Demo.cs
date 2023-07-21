50c35a3204d22923c9583bcc51785b88e7c8391c
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-21 08:55:40 +0530

    [Amit]Add : creating product review model class

---
+++ Lambda_LinQ_Demo/Lambda_LinQ_Demo/Lambda_Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda_LinQ_Demo
+{
+    //Lamda expression is used to create anonymos function , expression
+    //It is a sort code of block that accept parameter and return a value
+    //Use => as a lambda Operator which is used for lambda expression
+    internal class Lambda_Demo
+    {
+    }
+}
---
+++ Lambda_LinQ_Demo/Lambda_LinQ_Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lambda_LinQ_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<ProductReview> products = new List<ProductReview>();
+            {
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 2, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 3, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 4, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 5, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 6, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 7, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 8, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 9, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 10, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 11, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 12, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 13, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 14, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Average", IsLike = true }
+            }
+        }
+    }
+}
